4dfae0d0974772265034c4a84591ed16ffe1dc30
huey <index0327@naver.com>
2021-02-03 15:33:49 +0900

    delete

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -3,9 +3,6 @@ package com.git.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		System.out.println("샤샤샥");
-		System.out.println("Hi huey!");
 	}
 
 }
